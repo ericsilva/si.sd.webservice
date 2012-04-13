abe91093c34983ed8a08fc15457f5213d5c3f5f0
unknown <ericsilva@VF-TI-P00435.matriz.ce.vti.com.br>
2012-04-13 14:16:29 -0300

    Conf. diretorios da aplicacao

--- application/config/bootstrap.php
+++ application/config/bootstrap.php
@@ -2,11 +2,16 @@
 $path = dirname(__FILE__);
 $paths = array_unique(explode(PATH_SEPARATOR, get_include_path()));
 
-// Pastas da aplicacao, onde estao as classes
-$paths[] = $path . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'library';
-$paths[] = $path . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'controllers';
-$paths[] = $path . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'models';
-$paths[] = $path . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'views';
+//Pastas da aplicacao, onde estao as classes
+$applicationDirs = array(
+	'library',
+	'controllers',
+	'models',
+	'views');
+
+foreach($applicationDirs as $dir) {
+    $paths[] = $path . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $dir;
+}
 
 // Redefinindo o include path para as pastas da aplicacao
 set_include_path('.' . PATH_SEPARATOR . implode(PATH_SEPARATOR, $paths));
